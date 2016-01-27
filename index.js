33d866072b36a8c40bd772b1e7299e4c5c087090
Ubuntu <ubuntu@ip-172-31-27-112.us-west-2.compute.internal>
2016-01-27 20:52:52 +0000

    Attempted fix of html serving
    Get rid of that bad line about public
    
    change from sendFile to render
    
    remove slash
    
    changing back from render to sendFile
    
    added dirname to path of index.html
    
    Change to POST
    
    Only Create Game button
    
    Response for basic newgame
    
    handle GET requests
    
    Check GET query
    
    Check GET query again
    
    Check GET query try 3
    
    App.param to handle gameid
    
    App.param to handle gameid
    
    handle get with gameID and send back name of room
    
    Check that this isn't just a coincidence
    
    Cleanup

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.render("/index.html");
+	if(request.url.query == null || request.url.query == ""){
+ 		response.sendFile( __dirname + "/" + "index.html");
+	} else {
+		//TODO: Handle this error
+		response.send("error?");
+	}
 })
+
+app.param('gameid', function(request, response, next, gameid) {
+	//TODO: Check if the gameID is valid, if not, redirect back to empty request
+	response.send("welcome to room: " + gameid);
+	//TODO: Make a new Player and add them to the game, also send them all the info for the game they're in
+	next();
+});
+
+app.get('/:gameid', function(request, response) {
+	//TODO: Figure out if this function is necessary or what it does
+	response.send("welcome to game room?");
+});
+
+app.post('/newgame', function(request, response) {
+	//TODO: Check all existing games, create a new one and send the ID back as a redirect (Instead of /GameNumberUno)
+	response.redirect('/GAMeNUMeroUno');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
