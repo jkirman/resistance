0af55913bd369d96b5795590ec30ba0c9a98997a
Ubuntu <ubuntu@ip-172-31-27-112.us-west-2.compute.internal>
2016-01-28 04:08:22 +0000

    added parseInt() to comparison

--- index.js
+++ index.js
@@ -18,16 +18,9 @@ app.get('/', function(request, response) {
 app.param('gameid', function(request, response, next, gameid) {
     
     console.log("In param, the game id is " + gameid + "\nand the index it is at is " + gamelist.indexOf(gameid) + "and the contents of the list are now: " + gamelist.toString());
-    var found = false;
-    for(i in gamelist){
-	if (i == gameid){
-	 found = true;  
-	}
-    }
-    console.log("Was the gameid found: " + found);
-    
+        
     //Check if the gameID is valid, if not, redirect back to empty request
-    if(!found){
+    if(gamelist.indexOf(parseInt(gameid)) == -1){
 	response.redirect('/'); //THIS IS ALWAYS HAPPENING
     } else {    
 	response.send("welcome to room: " + gameid);
