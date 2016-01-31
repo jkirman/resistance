9ae2381030dd4605f8cded45fc17bf890821af2e
Ubuntu <ubuntu@ip-172-31-27-112.us-west-2.compute.internal>
2016-01-31 20:15:01 +0000

    css attempt 4

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 	}
 })
 
-app.use(express.static(path.join(__dirname, 'stylesheets')));
+app.use(express.static(path.join(__dirname, "stylesheets")));
 
 app.param('gameid', function(request, response, next, gameid) {
           
