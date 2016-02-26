aa0413fd0b7cd420296a0179952f2c0a4db32d09
cecilerobertmichon <cecilerobertm@gmail.com>
2016-02-26 16:07:05 +0000

    added display switch room

--- public/client.js
+++ public/client.js
@@ -4,12 +4,24 @@ var socket = io.connect();
 
 socket.on('connect', function() {
     var gameid = window.location.pathname.replace('/', '')
+    document.getElementById("link").innerHTML = window.location.href;
 	socket.emit("join", gameid)
 });
 
 socket.on('roomInfo', function(room) {
-    //document.write(JSON.stringify(room))
-    socket.emit("exampleClientEvent", "hello")
+
+    var plList = document.getElementById("playerList");
+    while (plList.hasChildNodes()) {   
+        plList.removeChild(plList.firstChild);
+    }
+
+    room.players.forEach(function(plName) {
+        var node = document.createElement("LI");                 // Create a <li> node
+        node.className = "list-group-item list-item-dark";
+        var textnode = document.createTextNode(plName);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        plList.appendChild(node);
+    })
 })
 
 socket.on("exampleServerEvent", function(data) {
@@ -27,14 +39,9 @@ socket.on('roomFull', function() {
     window.location = link;
 })
 
-socket.on('roomId', function(roomId) {
-    document.getElementById("link").innerHTML = window.location.href;
-})
-
-socket.on('newPlayer', function(player) {
-    var node = document.createElement("LI");                 // Create a <li> node
-    node.className = "list-group-item list-item-dark";
-    var textnode = document.createTextNode(player);         // Create a text node
-    node.appendChild(textnode);                              // Append the text to <li>
-    document.getElementById("playerList").appendChild(node);
-})
+document.getElementById("changeName-button").onclick = function() {
+    var newName = document.getElementById("changeName-text").value;
+    if(newName != "") {
+        socket.emit("changePlayerName", newName);
+    }
+}
